7dc27656f4e793d8c2e7a8da268d0782b834fc58
Sergii Pogrebniy <pogrebniy89@gmail.com>
2021-01-21 21:09:17 +0200

    day2 210121

--- script/script.js
+++ script/script.js
@@ -13,18 +13,23 @@ class FetchData {
 
 
 class Twitter {
-    constructor({listElem}) {
+    constructor({user, listElem, modalElems, tweetElems}) {
         const fetchData = new FetchData();
+        this.user = user;
         this.tweets = new Posts();
         this.elements = {
-            listElem: document.querySelector(listElem)
+            listElem: document.querySelector(listElem),
+            modal: modalElems,
+            tweetElems
         };
         fetchData.getPost()
             .then(data => {
                 data.forEach(this.tweets.addPost);
                 this.showAllPost()
             });
-        console.log(this.tweets, 'this.tweets');
+
+        this.elements.modal.forEach(this.handlerModal, this);
+        this.elements.tweetElems.forEach(this.addTweet, this);
     }
 
     renderPosts(tweets) {
@@ -45,11 +50,11 @@ class Twitter {
                             </header>
                             <div class="tweet-post">
                                 <p class="tweet-post__text">${text}</p>
-                                ${img ? 
-                                `<figure class="tweet-post__image">
+                                ${img ?
+                `<figure class="tweet-post__image">
                                 <img src="${img}" alt="илюстация поста ${nickname}">
                                 </figure>` :
-                                ""}
+                ""}
                             </div>
                         </div>
                     </div>
@@ -74,10 +79,66 @@ class Twitter {
         this.renderPosts(this.tweets.posts)
     }
 
-    openModal() {
+    handlerModal({button, modal, overlay, close}) {
+        const buttonElem = document.querySelector(button);
+        const modalElem = document.querySelector(modal);
+        const overlayElem = document.querySelector(overlay);
+        const closeElem = document.querySelector(close);
 
+        const openModal = () => {
+            modalElem.style.display = 'block';
+        }
+        const closeModal = (elem, event) => {
+            const target = event.target;
+            if (target === elem) {
+                modalElem.style.display = 'none';
+            }
+
+        }
+
+        buttonElem.addEventListener('click', openModal);
+
+        if (closeElem) {
+            closeElem.addEventListener('click', closeModal.bind(null, closeElem));
+        }
+
+        if (overlayElem) {
+            overlayElem.addEventListener('click', closeModal.bind(null, overlayElem));
+        }
+
+        this.handlerModal.closeModal = () => {
+            modalElem.style.display = 'none'
+        }
     }
 
+    addTweet({text, img, submit}) {
+        const textElem = document.querySelector(text);
+        const imgElem = document.querySelector(img);
+        const submitElem = document.querySelector(submit);
+
+        let imgUrl = '';
+        let tempString = textElem.innerHTML;
+
+        submitElem.addEventListener('click', () => {
+            this.tweets.addPost({
+                userName: this.user.name,
+                nickname: this.user.nick,
+                text: textElem.innerHTML,
+                img: imgUrl,
+            })
+            this.showAllPost();
+            this.handlerModal.closeModal();
+        })
+
+        textElem.addEventListener('click', () => {
+            if (textElem.innerHTML === tempString) {
+                textElem.innerHTML = '';
+            }
+        })
+        imgElem.addEventListener('click', () => {
+            imgUrl = prompt('Введи адрес картинки!')
+        })
+    }
 }
 
 class Posts {
@@ -86,7 +147,7 @@ class Posts {
     }
 
     addPost = (tweets) => {
-        this.posts.push(new Post(tweets));
+        this.posts.unshift(new Post(tweets));
     };
 
     deletePost(id) {
@@ -138,7 +199,26 @@ class Post {
 }
 
 const twitter = new Twitter({
-    listElem: '.tweet-list'
+    listElem: '.tweet-list',
+    user: {
+        name: 'Сергей',
+        nick: 'Pogrebniy'
+    },
+    modalElems: [
+        {
+            button: '.header__link_tweet',
+            modal: '.modal',
+            overlay: '.overlay',
+            close: '.modal-close__btn'
+        }
+    ],
+    tweetElems: [
+        {
+            text: '.modal .tweet-form__text',
+            img: '.modal .tweet-img__btn',
+            submit: '.modal .tweet-form__btn'
+        }
+    ]
 });
 
 console.log(new FetchData().getPost());
